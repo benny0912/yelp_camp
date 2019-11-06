1046abc27c062513865b0422b27729a53986ef77
benny0912 <bennykeanhoe.ooi@sjsu.edu>
2019-11-06 11:41:12 -0800

    added enviroment variable for database url

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 //var uri = "mongodb://localhost/yelp_camp";
-var uri = "mongodb+srv://benny0912:Pilot@1998@yelpcamp-nhwb3.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(uri);
+//var uri = "mongodb+srv://benny0912:Pilot@1998@yelpcamp-nhwb3.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(process.env.DATABASEURL);
 
 //seedDB();
 app.use(flash());
